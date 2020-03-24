af20912e8880208e5f20f42b97dc6b478a02a388
Adrian Price-Whelan <adrian.prw@gmail.com>
2020-03-24 14:52:51 -0400

    enable bash files changed

--- .circleci/files-changed.sh
+++ .circleci/files-changed.sh
@@ -4,6 +4,6 @@ if [[ -z $CIRCLE_PULL_REQUEST ]] ; then
     echo "Not a pull request"
 else
     echo $CIRCLE_PULL_REQUEST
-    FILES_CHANGED="$(git --no-pager diff --name-only FETCH_HEAD $(git merge-base FETCH_HEAD origin/master) '*.md')"
-    echo $FILES_CHANGED
+    export TUTORIALS_FILES_CHANGED="$(git --no-pager diff --name-only FETCH_HEAD $(git merge-base FETCH_HEAD origin/master) '*.md')"
+    echo $TUTORIALS_FILES_CHANGED
 fi
