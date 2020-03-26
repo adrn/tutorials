59fcaeb43b24a7609ecb75d6ebf71bd2ac08a1aa
Adrian Price-Whelan <adrian.prw@gmail.com>
2020-03-26 11:16:50 -0400

    orphan gh-pages branch

--- .circleci/gh-pages-push.sh
+++ .circleci/gh-pages-push.sh
@@ -2,18 +2,20 @@
 
 if [[ -z $CIRCLE_PULL_REQUEST ]] ; then
     echo "Not a pull request: pushing website to gh-pages branch."
-    git clone --single-branch -b gh-pages git@github.com:adrn/tutorials.git gh-pages
-    cp -r _build/* gh-pages
-    cp .gitignore gh-pages/
-    cp -R .circleci gh-pages/
+    git clone git@github.com:adrn/tutorials.git gh-pages
     cd gh-pages
+    git checkout --orphan gh-pages
+    rm -rf .
+    cp ../_build/* ./
+    cp ../.gitignore ./
+    cp -R ../_static ./
     git add .
     touch .nojekyll
     git add .nojekyll
     git status
     git -c user.name='circle' -c user.email='circle' commit -m "Upadate the build docs"
     git status
-    git push -q origin gh-pages
+    git push -qf origin gh-pages
 else
     echo $CIRCLE_PULL_REQUEST
     echo "This is a pull request: Not pushing to gh-pages!"
